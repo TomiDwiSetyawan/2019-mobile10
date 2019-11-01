5a3e129aa0ee95a1170249fea92f23059ebf9a16
Tomi Dwi Setyawan <tomidwisetyawan1@gmail.com>
2019-11-01 13:57:51 +0700

    Finish ViewModel Lanjut (Projeck SkorBasket)

--- SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/fragments/VisitorFragment.java
+++ SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/fragments/VisitorFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -34,6 +36,7 @@ public class VisitorFragment extends Fragment {
 
 	@Override
 	public void onCreate(@Nullable Bundle savedInstanceState) {
+		sharedScore = ViewModelProviders.of(requireActivity()).get(SharedScore.class);
 		super.onCreate(savedInstanceState);
 	}
 
@@ -42,6 +45,7 @@ public class VisitorFragment extends Fragment {
 							 Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
 		return inflater.inflate(R.layout.fragment_visitor, container, false);
+
 	}
 
 	@Override
@@ -52,7 +56,35 @@ public class VisitorFragment extends Fragment {
 		scoreVisitor = view.findViewById(R.id.scoreVisitor);
 		scoreSatuVisitor = view.findViewById(R.id.scoreSatuVisitor);
 
-
 		// Tambahkan logic tombol di bagian bawah ini
+		sharedScore.getScoreVisitor().observe(requireActivity(), new Observer<Integer>() {
+			@Override
+			public void onChanged(Integer score) {
+				scoreVisitor.setText(String.valueOf(score));
+				scoreDefault = score;
+			}
+		});
+
+		scoreDuaVisitor.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				sharedScore.setScoreVisitor(scoreDefault+2);
+			}
+		});
+
+		scoreTigaVisitor.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				sharedScore.setScoreVisitor(scoreDefault+3);
+			}
+		});
+
+		scoreSatuVisitor.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				sharedScore.setScoreVisitor(scoreDefault+1);
+			}
+		});
+
 	}
 }
